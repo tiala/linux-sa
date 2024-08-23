94e94e89d3d5e0b771bb9e92f0870458d3ef302f
Yunhong Jiang <yunhong.jiang@linux.intel.com>
2024-08-23 16:23:25 -0700

    x86/hyperv/vtl: Set real_mode_header in hv_vtl_init_platform()
    Hyper-V VTL clears x86_platform.realmode_{init(), reserve()} in
    hv_vtl_platform_init() whereas it sets real_mode_header later in
    hv_vtl_early_init(). There is no need to deal with the real mode memory
    in two places: x86_platform.realmode_init() is invoked much later via an
    early_initcall.
    
    Set real_mode_header in hv_vtl_init_platform() to keep all code dealing
    with memory for the real mode trampoline in one place. Besides making the
    code more readable, it prepares it for a subsequent changeset in which the
    behavior needs to change to support Hyper-V VTL guests in TDX environment.
    
    Reviewed-by: Michael Kelley <mhklinux@outlook.com>
    Suggested-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Yunhong Jiang <yunhong.jiang@linux.intel.com>
    Signed-off-by: Ricardo Neri <ricardo.neri-calderon@linux.intel.com>

--- arch/x86/hyperv/hv_vtl.c
+++ arch/x86/hyperv/hv_vtl.c
@@ -42,6 +42,7 @@ void __init hv_vtl_init_platform(void)
 	x86_init.resources.probe_roms = x86_init_noop;
 	x86_platform.realmode_reserve = x86_init_noop;
 	x86_platform.realmode_init = x86_init_noop;
+	real_mode_header = &hv_vtl_real_mode_header;
 	x86_init.irqs.pre_vector_init = x86_init_noop;
 	x86_init.timers.timer_init = x86_init_noop;
 	x86_init.resources.probe_roms = x86_init_noop;
