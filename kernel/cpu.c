romank-msft/OHCL-Linux-Kernel

kernel: add boot_cpus command line parameter
Add a command line parameter, "boot_cpus", for specifying the set of
CPUs to online during boot.  CPUs that are not specified are still
marked present and can be onlined separately after boot.

This is essentially an extension of the existing maxcpus parameter.
maxcpus can limit the number of boot CPUs, but it always chooses the
first N CPUs (except on platforms with SMT enabled, where it will choose
the first thread of the first N cores).  This new parameter allows
specifying precisely which CPUs should start.  This is useful if the
online processors should include CPUs from specific NUMA nodes, for
device or memory locality reasons.

Without this change, a user can use maxcpus and then online the other
desired processors after boot. This is slow on large machines, and
it operates poorly with some drivers (e.g., those that make policy
decisions at startup based on cpu_online_mask). It is better to specify
the desired CPU set at boot.

Signed-off-by: John Starks <jostarks@microsoft.com>

kernel/cpu.c
@@ -1866,10 +1866,8 @@ static inline const struct cpumask *cpuhp_get_primary_thread_mask(void)
  * This avoids waiting for each AP to respond to the startup IPI in
  * CPUHP_BRINGUP_CPU.
  */
-static bool __init cpuhp_bringup_cpus_parallel(unsigned int ncpus)
+static bool __init cpuhp_bringup_cpus_parallel(const struct cpumask *mask, unsigned int ncpus)
 {
-	const struct cpumask *mask = cpu_present_mask;
-
 	if (__cpuhp_parallel_bringup)
 		__cpuhp_parallel_bringup = arch_cpuhp_init_parallel_bringup();
 	if (!__cpuhp_parallel_bringup)
@@ -1905,17 +1903,25 @@ static bool __init cpuhp_bringup_cpus_parallel(unsigned int ncpus)
 static inline bool cpuhp_bringup_cpus_parallel(unsigned int ncpus) { return false; }
 #endif /* CONFIG_HOTPLUG_PARALLEL */
 
-void __init bringup_nonboot_cpus(unsigned int setup_max_cpus)
+void __init bringup_nonboot_cpus(unsigned int setup_max_cpus, const struct cpumask *boot_cpus)
 {
+	const struct cpumask *mask = cpu_present_mask;
+
+	if (boot_cpus) {
+		static struct cpumask tmp_mask __initdata;
+		cpumask_and(&tmp_mask, mask, boot_cpus);
+		mask = &tmp_mask;
+	}
+
 	if (!setup_max_cpus)
 		return;
 
 	/* Try parallel bringup optimization if enabled */
-	if (cpuhp_bringup_cpus_parallel(setup_max_cpus))
+	if (cpuhp_bringup_cpus_parallel(mask, setup_max_cpus))
 		return;
 
 	/* Full per CPU serialized bringup */
-	cpuhp_bringup_mask(cpu_present_mask, setup_max_cpus, CPUHP_ONLINE);
+	cpuhp_bringup_mask(mask, setup_max_cpus, CPUHP_ONLINE);
 }
 
 #ifdef CONFIG_PM_SLEEP_SMP

tiala/linux-sa

kernel/cpu.c
@@ -1839,10 +1839,8 @@ bool __weak arch_cpuhp_init_parallel_bringup(void)
  * This avoids waiting for each AP to respond to the startup IPI in
  * CPUHP_BRINGUP_CPU.
  */
-static bool __init cpuhp_bringup_cpus_parallel(unsigned int ncpus)
+static bool __init cpuhp_bringup_cpus_parallel(const struct cpumask *mask, unsigned int ncpus)
 {
-	const struct cpumask *mask = cpu_present_mask;
-
 	if (__cpuhp_parallel_bringup)
 		__cpuhp_parallel_bringup = arch_cpuhp_init_parallel_bringup();
 	if (!__cpuhp_parallel_bringup)
@@ -1875,20 +1873,29 @@ static bool __init cpuhp_bringup_cpus_parallel(unsigned int ncpus)
 	return true;
 }
 #else
-static inline bool cpuhp_bringup_cpus_parallel(unsigned int ncpus) { return false; }
+static inline bool cpuhp_bringup_cpus_parallel(const struct cpumask *mask, unsigned int ncpus) { return false; }
 #endif /* CONFIG_HOTPLUG_PARALLEL */
 
-void __init bringup_nonboot_cpus(unsigned int max_cpus)
+void __init bringup_nonboot_cpus(unsigned int max_cpus, const struct cpumask *boot_cpus)
 {
+	const struct cpumask *mask = cpu_present_mask;
+
+	if (boot_cpus) {
+		static struct cpumask tmp_mask __initdata;
+
+		cpumask_and(&tmp_mask, mask, boot_cpus);
+		mask = &tmp_mask;
+	}
+
 	if (!max_cpus)
 		return;
 
 	/* Try parallel bringup optimization if enabled */
-	if (cpuhp_bringup_cpus_parallel(max_cpus))
+	if (cpuhp_bringup_cpus_parallel(mask, max_cpus))
 		return;
 
 	/* Full per CPU serialized bringup */
-	cpuhp_bringup_mask(cpu_present_mask, max_cpus, CPUHP_ONLINE);
+	cpuhp_bringup_mask(mask, setup_max_cpus, CPUHP_ONLINE);
 }
 
 #ifdef CONFIG_PM_SLEEP_SMP