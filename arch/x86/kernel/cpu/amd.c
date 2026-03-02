c39b2e37a2b3b381cadcd011db3a4c627ae3d092
Tianyu Lan <tiala@microsoft.com>
2026-03-02 08:12:38 -0500

    x86/AMD: Disable INVLPGB capability to avoid GP fault
    Commit 4afeb0ed1753 introduces "broadcast TLB
    invalidation for multi-threaded processes" which
    uses INVLPGB to flush tlb on all cpus. The
    instruction may trigger GP fault and Underhill
    fails to boot up. The follow log shows the error
    and disable INVLPGB function.
    
    <31>[    4.060864] hcl::ioctl::snp: DEBUG  pvalidate range=0x5600000-0x5c00000 validate=true terminate_on_failure=false
    ....
    <4>[    5.681406] Oops: general protection fault, maybe for address 0xffffffff88751881: 0000 [#1] SMP
    <4>[    5.686839] CPU: 23 UID: 0 PID: 263 Comm: worker-Underhil Tainted: G S                  6.18.0-microsoft-hcl+ #11 PREEMPT(none)
    <4>[    5.694075] Tainted: [S]=CPU_OUT_OF_SPEC
    <4>[    5.696600] RIP: 0010:flush_tlb_mm_range+0xb4/0x500
    <4>[    5.700247] Code: 00 00 41 0f b7 95 5c 04 00 00 4d 8b 44 24 10 83 c2 01 4d 8b 2c 24 c1 e2 10 49 83 f8 ff 0f 85 4b 02 00 00 b8 06 00 00 00 31 c9 <0f> 01 fe 0f 1f 44 00 00 41 0f b7 85 5c 04 00 00 66 89 45 d4 0f 1f
    <4>[    5.712245] RSP: 0018:ffffc9000a837860 EFLAGS: 00010246
    <4>[    5.716438] RAX: 0000000000000006 RBX: ffff88800d278000 RCX: 0000000000000000
    <4>[    5.720911] RDX: 0000000000070000 RSI: ffffffff88751881 RDI: ffffffff88759c3e
    <4>[    5.724861] RBP: ffffc9000a837898 R08: ffffffffffffffff R09: 000000000000006b
    <4>[    5.728857] R10: 000000008040003b R11: ffff88800ea9920c R12: ffff88802f3e0d00
    <4>[    5.733060] R13: ffff88800d278000 R14: 0000000000000017 R15: 00007fd836836000
    <4>[    5.736377] FS:  00007fd836835b38(0000) GS:ffff8880a69ed000(0000) knlGS:0000000000000000
    <4>[    5.740371] CS:  0010 DS: 0000 ES: 0000 CR0: 0000000080050033
    <4>[    5.744179] CR2: 0000042e18091000 CR3: 000800000e4d4000 CR4: 0000000000310eb0
    <4>[    5.748362] Call Trace:
    <4>[    5.752065]  <TASK>
    <4>[    5.753474]  tlb_finish_mmu+0xd3/0x150
    <4>[    5.755874]  vms_clear_ptes+0x141/0x150
    <4>[    5.758258]  ? downgrade_write+0x5e/0xf0
    <4>[    5.760682]  vms_complete_munmap_vmas+0x64/0x160
    <4>[    5.762434]  do_vmi_align_munmap+0x18b/0x1c0
    <4>[    5.764049]  do_vmi_munmap+0xd2/0x150
    <4>[    5.766104]  __vm_munmap+0xbe/0x120
    <4>[    5.768767]  __x64_sys_munmap+0x16/0x20
    <4>[    5.771436]  x64_sys_call+0x1d3b/0x2190
    <4>[    5.773978]  do_syscall_64+0x79/0x2d0
    <4>[    5.776570]  ? _raw_spin_unlock_irq+0x15/0x40
    <4>[    5.779644]  ? __set_current_blocked+0x41/0x50
    <4>[    5.782478]  ? sigprocmask+0x6e/0x90
    <4>[    5.784797]  ? _copy_to_user+0x2e/0x50
    <4>[    5.787169]  ? __x64_sys_rt_sigprocmask+0x8a/0xd0
    <4>[    5.790111]  ? x64_sys_call+0x19c0/0x2190
    <4>[    5.792682]  ? do_syscall_64+0x79/0x2d0
    <4>[    5.795136]  ? x64_sys_call+0x19c0/0x2190
    <4>[    5.797894]  ? do_syscall_64+0x79/0x2d0
    <4>[    5.800254]  ? do_vmi_align_munmap+0x18b/0x1c0
    <4>[    5.801952]  ? do_vmi_munmap+0xd2/0x150
    <4>[    5.803462]  ? __vm_munmap+0xbe/0x120
    <4>[    5.804922]  ? __x64_sys_munmap+0x16/0x20
    <4>[    5.807556]  ? x64_sys_call+0x1d3b/0x2190
    <4>[    5.810120]  ? do_syscall_64+0x79/0x2d0
    <4>[    5.812541]  entry_SYSCALL_64_after_hwframe+0x76/0x7e
    <4>[    5.814460] RIP: 0033:0x7fd83f3d53b7
    <4>[    5.815876] Code: 00 00 00 ba ff ff ff 7f be 81 00 00 00 4c 89 c0 0f 05 48 83 f8 da 75 d8 be 01 00 00 00 4c 89 c0 0f 05 c3 b8 0b 00 00 00 0f 05 <48> 31 ff b8 3c 00 00 00 0f 05 31 c0 b0 38 49 89 fb 48 89 d7 4c 89
    <4>[    5.825761] RSP: 002b:00007fd8368357d8 EFLAGS: 00000246 ORIG_RAX: 000000000000000b
    <4>[    5.829169] RAX: ffffffffffffffda RBX: 00007fd836835b38 RCX: 00007fd83f3d53b7
    <4>[    5.833375] RDX: 00007fd8368357e0 RSI: 0000000000203000 RDI: 00007fd836633000
    <4>[    5.838527] RBP: 0000000000000003 R08: 00007fd836835bc0 R09: 0000000040000000
    <4>[    5.843255] R10: 0000000000000008 R11: 0000000000000246 R12: 00007fd8368357e0
    <4>[    5.847879] R13: 00007fd836835b70 R14: 00000000000000ca R15: 0000000000000001
    <4>[    5.852645]  </TASK>
    <4>[    5.854394] Modules linked in: hv_storvsc pci_hyperv pci_hyperv_intf
    
    Signed-off-by: Tianyu Lan <tiala@microsoft.com>

--- arch/x86/kernel/cpu/amd.c
+++ arch/x86/kernel/cpu/amd.c
@@ -1184,6 +1184,9 @@ static void init_amd(struct cpuinfo_x86 *c)
 	/* Enable Translation Cache Extension */
 	if (cpu_has(c, X86_FEATURE_TCE))
 		msr_set_bit(MSR_EFER, _EFER_TCE);
+
+	/* Correct misconfigured CPUID on some clients. */
+	clear_cpu_cap(c, X86_FEATURE_INVLPGB);
 }
 
 #ifdef CONFIG_X86_32
